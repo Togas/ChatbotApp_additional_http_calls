dc8acade3b9c6c556f963d34f6b970446ee3bcfa
Ben Stucke <ben-shuang-feng.stucke@adesso.de>
2019-11-09 21:29:43 +0100

    deleted unneccesary interfaces

--- src/main/java/stucke/chatbot/db/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package stucke.chatbot.db;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import stucke.chatbot.model.collections.Answer;
-
-public interface AnswerRepository extends MongoRepository<Answer, String> {
-}
--- src/main/java/stucke/chatbot/db/ChatContextRepository.java
+++
@@ -1,9 +0,0 @@
-package stucke.chatbot.db;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import stucke.chatbot.model.collections.ChatContext;
-
-@Repository
-public interface ChatContextRepository extends MongoRepository<ChatContext, String> {
-}
